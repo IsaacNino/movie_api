528706fc12653549987704ece696b864bcde394d
Isaac Nino <114028749+IsaacNino@users.noreply.github.com>
2023-03-18 16:09:33 -0500

    created schemas and exported them
    added, changed /models.js

---
+++ models.js
@@ -0,0 +1,34 @@
+const { default: mongoose } = require('mongoose');
+
+let movieSchema = mongoose.Schema({ // Schema for movies
+    Title: {type: String, required: true}, // Title of movie
+    Description: {type: String, required: true}, // Description of movie
+    Genre: { // Genre of movie
+        Name: String, // Name of genre
+        Description: String // Description of genre
+    },
+    Director: { // Director of movie
+        Name: String, // Name of director
+        Bio: String, // Bio of director
+    },
+    Actors: [String], // Actors in movie
+    ImagePath: String, // Image of movie
+    Featured: Boolean // Featured movie
+});
+
+let userSchema = mongoose.Schema({ // Schema for users 
+    Username: {type: String, required: true}, // Username of user is required
+    Password: {type: String, required: true}, // Password of user is required
+    Email: {type: String, required: true}, // Email of user is required
+    Birthday: Date, // Birthday of user
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}] // Favorite movies of user
+});
+
+// creation of models
+let Movie = mongoose.model('Movie', movieSchema); // Movie model
+let User = mongoose.model('User', userSchema); // User model
+
+
+//export models
+module.exports.Movie = Movie; // Export movie model
+module.exports.User = User; // Export user model
