a1b1a10eb9aa0cf3a1db8b6bc4f48bc27a54516d
Isaac Nino <114028749+IsaacNino@users.noreply.github.com>
2023-03-29 19:11:07 -0500

    added console.log function to catch an error
    changed /auth.js

--- auth.js
+++ auth.js
@@ -16,7 +16,8 @@ let generateJWTToken = (user) => { // Create a function that generates a JWT
 // POST login
 module.exports = (router) => {
     router.post('/login', (req, res) => { //When a POST request is made to the /login endpoint
-      passport.authenticate('local', { session: false }, (error, user, info) => { //Authenticate the user using the local strategy
+      passport.authenticate('local', { session: false }, (user, error, info) => { //Authenticate the user using the local strategy
+        console.log(error);
         if (error || !user) { //If there is an error or the user is not found
           return res.status(400).json({ //Return a 400 status code and a JSON object with the error message
             message: 'Something is not right', //The error message
